e3bf8b1a48d6dd4f4190c6ad85a2fe6fff737c03
Charles Cheung <charlescheung22@gmail.com>
2023-08-21 03:06:43 -0400

    Added docstrings and changed movement

--- src/bouncing dvd.py
+++ src/bouncing dvd.py
@@ -2,8 +2,10 @@ from math import cos, radians, pi
 import pygame
 
 # Constants
-DVD_LOGO_PATH = "../images/DVD logo.png"
-RGB_LIST = [(
+FPS: int = 144
+DVD_LOGO_PATH: str = "../images/DVD logo.png"
+# Generate a list of RGB values for background color cycling.
+RGB_LIST: list[tuple[int, int, int]] = [(
     int(max(255 / 3 * (cos(3 * radians(degree) + 0 * pi / 3) + 1.5), 0)),
     int(max(255 / 3 * (cos(3 * radians(degree) + 2 * pi / 3) + 1.5), 0)),
     int(max(255 / 3 * (cos(3 * radians(degree) + 4 * pi / 3) + 1.5), 0))
@@ -30,12 +32,20 @@ class Logo:
         # Collision detection will be handled in the main loop
 
     def draw(self, surface: pygame.Surface) -> None:
-        """Draw the DVD logo's sprite to the surface provided, at the logo's current position."""
+        """
+        Draw the DVD logo on a given surface.
+
+        :param surface: Pygame surface to draw the logo on.
+        """
         surface.blit(self.sprite, self.position)
 
 
 def handle_events() -> str:
-    """Asks for pygame events and keys pressed to know when to quit."""
+    """
+    Handle Pygame events and determine whether to continue the screensaver or quit.
+
+    :return: Either "Screensaver" to continue or "Quit" to exit.
+    """
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             return "Quit"
@@ -47,8 +57,11 @@ def handle_events() -> str:
 
 def main():
     pygame.init()
-    curr_rgb = 0  # 0 <= curr_rgb < 360
+    curr_rgb = 0  # RGB index for background color cycling.
 
+    flag_bounced: bool = False
+
+    # Getting screen dimensions
     display_info = pygame.display.Info()
     screen_width = display_info.current_w
     screen_height = display_info.current_h
@@ -63,26 +76,30 @@ def main():
         # Handle events
         mode = handle_events()
 
-        # Move
         dvd_icon.update()
-        # Handle collision with boundaries
+        # Check for collisions with screen boundaries and reverse direction if necessary
         if dvd_icon.position.x <= 0 or dvd_icon.position.x >= dvd_bound_x:
             dvd_icon.direction.x *= -1
+            flag_bounced = True
         if dvd_icon.position.y <= 0 or dvd_icon.position.y >= dvd_bound_y:
             dvd_icon.direction.y *= -1
-        # Move into boundaries
-        dvd_icon.update()
+            flag_bounced = True
 
-        # Drawing to screen
+        # Update position after handling collisions
+        if flag_bounced:
+            dvd_icon.update()
+            flag_bounced = False
+
+        # Render DVD logo and background
         main_surface.fill(RGB_LIST[int(curr_rgb)])
         dvd_icon.draw(main_surface)
-
         pygame.display.flip()
 
         curr_rgb += 1 / 20
         curr_rgb %= 360
 
     pygame.quit()
+
 
 
 if __name__ == "__main__":
